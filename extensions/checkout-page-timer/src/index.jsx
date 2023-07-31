12630d534c1a980ee1785de6a1579ea4f1e55ef0
abdullahkhan2001 <abdullahkhan.ak2001@gmail.com>
2023-07-31 13:13:43 +0500

    add file

--- extensions/checkout-page-timer/src/index.jsx
+++ extensions/checkout-page-timer/src/index.jsx
@@ -1,102 +1,102 @@
-import React from 'react';
-import {
-  render,
-  BlockStack,
-  TextBlock,
-  useSettings,
-  View,
-  Text,
-} from '@shopify/checkout-ui-extensions-react';
+import React, { useEffect, useState } from 'react';
+import { render, BlockStack, Text, useSettings, View, useExtensionApi } from '@shopify/checkout-ui-extensions-react';
 
 render('Checkout::Dynamic::Render', () => <App />);
 
-function App() {
-  const {timer_before_text, timer_after_text, timer} = useSettings();
-  const {timer_text_color, timer_text_size, timer_size, timer_color} = useSettings()
-  const initialSeconds = timer * 60; // 15 minutes in seconds
-  const [seconds, setSeconds] = React.useState(initialSeconds);
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  const formattedMinutes = minutes.toString().padStart(2, '0');
+  const formattedSeconds = seconds.toString().padStart(2, '0');
+  return `${formattedMinutes}:${formattedSeconds}`;
+}
 
-  React.useEffect(() => {
+function App() {
+  const {
+    timer_before_text,
+    timer_after_text,
+    timer,
+    timer_ends,
+    timer_text_color,
+    timer_text_size,
+    timer_size,
+    timer_color
+  } = useSettings();
+
+  const { storage } = useExtensionApi(); // Obtaining the storage object from the extension API
+  const [counterTimer, setCounterTimer] = useState(timer * 60); // Initial timer value set to the value from useSettings()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const storedTimer = await storage.read('countdownTimer');
+        if (storedTimer !== null) {
+          setCounterTimer(storedTimer);
+        }
+      } catch (error) {
+        console.error('Error while reading timer data:', error);
+      }
+    };
+
+    fetchData();
+  }, [storage]);
+
+  useEffect(() => {
     const interval = setInterval(() => {
-      setSeconds((prevSeconds) => {
-        if (prevSeconds <= 0) {
-          clearInterval(interval); // Stop the timer
+      setCounterTimer((prevTimer) => {
+        if (prevTimer > 0) {
+          const newTimer = prevTimer - 1;
+          storage
+            .write('countdownTimer', newTimer)
+            .catch((error) => console.error('Error while writing timer data:', error));
+          return newTimer;
+        } else {
+          clearInterval(interval);
+          storage
+            .delete('countdownTimer')
+            .catch((error) => console.error('Error while deleting timer data:', error));
           return 0;
         }
-        return prevSeconds - 1;
       });
     }, 1000);
 
     return () => {
       clearInterval(interval);
     };
-  }, []);
+  }, [storage]);
 
-  const formattedTime = React.useMemo(() => {
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = seconds % 60;
-    return `${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
-  }, [seconds]);
-
-  const minutes = Math.floor(seconds / 60);
-  const remainingSeconds = seconds % 60;
+  const formattedTime = formatTime(counterTimer);
 
   return (
-    
-    <BlockStack 
-    border={'base'}
-    borderRadius={'base'}
-    padding={'tight'}
-    spacing={'loose'}
-    blockAlignment={'center'}
-    cornerRadius={'loose'}
-    overflow='hidden'
-    inlineAlignment={'center'}>
-      <TextBlock>
-    <View>
-      <Text size={timer_text_size} appearance={timer_text_color} emphasis='bold'>{timer_before_text}</Text>
-      <Text size={timer_size} emphasis='bold' appearance={timer_color} > {minutes} : {remainingSeconds}</Text>
-      <Text size={timer_text_size} appearance={timer_text_color} emphasis='bold'> {timer_after_text}</Text></View>
-    </TextBlock>
+    <BlockStack
+      border="base"
+      borderRadius="base"
+      padding="tight"
+      spacing="loose"
+      blockAlignment="center"
+      cornerRadius="loose"
+      overflow="hidden"
+      inlineAlignment="center"
+    >
+      <View>
+        <Text size={timer_text_size} appearance={timer_text_color} emphasis="strong">
+          {timer_before_text}
+        </Text>
+        {counterTimer > 0 ? (
+          <Text size={timer_size} emphasis="strong" appearance={timer_color}>
+            {formattedTime}
+          </Text>
+        ) : (
+          <Text size={timer_size} appearance={timer_color} emphasis="strong">
+            {timer_ends}
+          </Text>
+        )}
+        <Text size={timer_text_size} appearance={timer_text_color} emphasis="strong">
+          {timer_after_text}
+        </Text>
+      </View>
     </BlockStack>
-    
   );
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import {
-//   useExtensionApi,
-//   render,
-//   Banner,
-//   useTranslate,
-// } from '@shopify/checkout-ui-extensions-react';
-
-// render('Checkout::Dynamic::Render', () => <App />);
-
-// function App() {
-//   const {extensionPoint} = useExtensionApi();
-//   const translate = useTranslate();
-//   return (
-//     <Banner title="checkout-timer">
-//       {translate('welcome', {extensionPoint})}
-//     </Banner>
-//   );
-// }
